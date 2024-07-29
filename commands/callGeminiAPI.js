16e35bb0016d001c1af5f13ff5e13e2381f24fa7
TeacherMada <99089646+TeacherMada@users.noreply.github.com>
2024-07-29 21:44:23 +0300

    Update callGeminiAPI.js

--- commands/callGeminiAPI.js
+++ commands/callGeminiAPI.js
@@ -2,7 +2,8 @@ const request = require('request');
 
 function callGeminiAPI(prompt) {
   return new Promise((resolve, reject) => {
-    const apiUrl = `https://gemini-yvcl.onrender.com/api/ai/chat?prompt=${encodeURIComponent(prompt)}&id=40`;
+    const apiUrl = `https://hazee-gemini-ultra-c1ad28d5e422.herokuapp.com/bard?question=${encodeURIComponent(prompt)}`;
+      //`https://gemini-yvcl.onrender.com/api/ai/chat?prompt=${encodeURIComponent(prompt)}&id=40`;
     
     request(apiUrl, (error, response, body) => {
       if (error) {
