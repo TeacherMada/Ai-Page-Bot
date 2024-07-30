09a80998f0e8372b3695ae159ce4a21ac666a0fe
TeacherMada <99089646+TeacherMada@users.noreply.github.com>
2024-07-30 09:35:11 +0300

    Update ai.js

--- commands/ai.js
+++ commands/ai.js
@@ -1,11 +1,10 @@
-const fetch = require('node-fetch');
-
 async function callAI(query) {
     const apiKey = 'j86bwkwo-8hako-12C';
     const encodedQuery = encodeURIComponent(query);
     const apiUrl = `https://liaspark.chatbotcommunity.ltd/@unregistered/api/0.5894690218095953?key=${apiKey}&query=${encodedQuery}`;
 
     try {
+        const fetch = (await import('node-fetch')).default;
         const response = await fetch(apiUrl);
         if (!response.ok) {
             throw new Error(`Error: ${response.statusText}`);
